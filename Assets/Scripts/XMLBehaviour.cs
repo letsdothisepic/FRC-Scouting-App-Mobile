9759c272ab8730a9a4dbb0939fd7e0adafb7ef93
Colin Harris <letsdothisgamez@gmail.com>
2018-09-16 21:43:20 -0400

    Initial Commit Progress - Basic layout for Proof of Concept, checkboxes for Auton have functionality and the XML compiler (when running) has acess to their values. Scrolling mechanics added, and in the process of fixing scaling across devices issue.

---
+++ Assets/Scripts/XMLBehaviour.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class XMLBehaviour : MonoBehaviour {
+    [SerializeField] Toggle wiggleandnothingToggle;
+    [SerializeField] Toggle robotdoesnothingToggle;
+    [SerializeField] Toggle baselineToggle;
+    [SerializeField] Toggle baselinefailToggle;
+    [SerializeField] Toggle switchToggle;
+    [SerializeField] Toggle switchfailToggle;
+    [SerializeField] Toggle scaleToggle;
+    [SerializeField] Toggle scalefailToggle;
+    [SerializeField] Toggle multipleonswitchToggle;
+    [SerializeField] Toggle twoonscaleToggle;
+    //There is currently one checkboxe that does not serve a purpose, add it here once done.
+    // Use this for initialization
+    void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void WiggleandnothingUpdate()
+    {
+        if (wiggleandnothingToggle.isOn)
+        {
+            print("On!");
+        }
+        else
+        {
+            print("Off!");
+        }
+    }
+    public void RobotdoesnothingUpdate()
+    {
+        if (robotdoesnothingToggle.isOn)
+        {
+            print("On!");
+        }
+        else
+        {
+            print("Off!");
+        }
+    }
+    public void BaselineUpdate()
+    {
+        if (baselineToggle.isOn)
+        {
+            print("On!");
+        }
+        else
+        {
+            print("Off!");
+        }
+    }
+    public void BaselinefailUpdate()
+    {
+        if (baselinefailToggle.isOn)
+        {
+            print("On!");
+        }
+        else
+        {
+            print("Off!");
+        }
+    }
+    public void SwitchUpdate()
+    {
+        if (switchToggle.isOn)
+        {
+            print("On!");
+        }
+        else
+        {
+            print("Off!");
+        }
+    }
+    public void SwitchfailUpdate()
+    {
+        if (switchfailToggle.isOn)
+        {
+            print("On!");
+        }
+        else
+        {
+            print("Off!");
+        }
+    }
+    public void ScaleUpdate()
+    {
+        if (scaleToggle.isOn)
+        {
+            print("On!");
+        }
+        else
+        {
+            print("Off!");
+        }
+    }
+    public void ScalefailUpdate()
+    {
+        if (scalefailToggle.isOn)
+        {
+            print("On!");
+        }
+        else
+        {
+            print("Off!");
+        }
+    }
+    public void MultipleonswitchUpdate()
+    {
+        if (multipleonswitchToggle.isOn)
+        {
+            print("On!");
+        }
+        else
+        {
+            print("Off!");
+        }
+    }
+    public void TwoonscaleUpdate()
+    {
+        if (twoonscaleToggle.isOn)
+        {
+            print("On!");
+        }
+        else
+        {
+            print("Off!");
+        }
+    }
+    //The same applies down here, make a new void for the new update.
+}
